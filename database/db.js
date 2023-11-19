0ba9e86a64793a4f1c7ed314d20718475d9fc32a
augustonks <augustomatias2019@outlook.com>
2023-11-19 17:14:27 -0300

    ejs componentization

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = () => {
     mongoose
         .connect(
-            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database.9bs11pv.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.DATABASE_NAME}.9bs11pv.mongodb.net/?retryWrites=true&w=majority`
         )
         .then(() => console.log("CONECTADO AO MONGO"))
         .catch((err) => console.log("erro ao conectar-se ao mongo: ", err));
